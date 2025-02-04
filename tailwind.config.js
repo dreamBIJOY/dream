6e48127b9cb615eda443fd9d6b89cfdae89e8992
dreamBIJOY <dreambijoyinfo@yahoo.com>
2025-02-04 09:48:02 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily:{
+        liter: ['Liter', 'serif'],
+        roboto: ['Roboto', 'serif'],
+        anton: ['Anton', 'serif'],
+        oleo: ['Oleo Script', 'serif'],
+      },
+      colors:{
+        'bg-color': '#09122C',
+        
+      },
+      width:{
+
+      }
+    },
+  },
+  plugins: [],
+}
+
